86da649ea899e9317df147e5706f877077cf87a2
jotrumo <jtmontoya26@gmail.com>
2020-05-23 12:44:57 -0700

    Quick Save

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -16,13 +16,32 @@ var MessagesView = {
     //if data.results we will iterate over the length of data.
     console.log(data);
     if(data.results) {
+      var roomObj ={};
+      var roomArray=[];
       for (var i = 0; i < data.results.length; i++) {
         //#chats append//from here we are going to call messege view and its going to return messages.
-
+        // RoomsView.initialize(data.results[i]);
         MessagesView.$chats.append(MessageView.render(data.results[i]));
-        //Roomsview.render(data.results[i].roomname);
+        roomObj[data.results[i].roomname] = 0;
+
+        for(var key in roomObj)
+        {
+          roomArray.push(JSON.stringify(key))
+        }
+
+        for(var j = 0; j<roomArray.length; j++){
+          RoomsView.render(roomArray[i]);
+        }
       }
     }
   }
 
 };
+
+
+
+    // roomArray = Object.keys(roomObj);
+    // for(var j =0; j<roomArray.length; j++)
+    // {
+    //   RoomsView.render(roomArray[j]);
+    // }
--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -3,10 +3,19 @@ var RoomsView = {
   $button: $('#rooms button'),
   $select: $('#rooms select'),
 
-  initialize: function() {
+
+
+  initialize: function(room) {
+
   },
 
-  render: function() {
+  render: function(roomname)
+  {
+    if(roomname !== "undefined"){
+      var room= JSON.parse(roomname);
+    };
+    RoomsView.$select.empty();
+    RoomsView.$select.append(`<option id= ${room} value = ${room}>${room}</option>`)
   }
 
 };
